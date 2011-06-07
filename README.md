a668483e50ef17c20dc899e0f1392a05a81d5c60
Brandon Toone <xsonih@gmail.com>
2011-06-07 12:50:00 -0600

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-TODO: Add description to README
+TODO:
+
+* create two branches; one for connecting the app to a local mongo db and one for connecting the app to development database on mongohq
+* see if it's possible to have the app connect to two databased at mongohq (1 for dev, one for production) using heroku
+* figure out how to have a sinatra/mongo app that runs locally and on heroku
--- app.rb
+++ app.rb
@@ -1,16 +1,44 @@
 require 'sinatra'
 require 'mongo'
-require 'uri'
+require 'yaml'
 
 configure do
-    uri = URI.parse(ENV['MONGOHQ_URL'])
-    # you don't need to specify auth because it's already in the env var
-    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])    
-    db = conn.db(uri.path.gsub(/^\//, ''))
-    
-    # access a specific collection
-    set :db, db
-    set :coll, db.collection("your_collection")
+  # p ENV['RACK_ENV']
+  # p settings.environment
+  # config = YAML::load(File.read('config/database.yml'))
+  # 
+  # db = Mongo::Connection.new(config['heroku']['host'], config['heroku']['port']).db(config['heroku']['database'])
+  # auth = db.authenticate(config['heroku']['username'], config['heroku']['password'])
+  # set :mongo_db, db
+  
+  # # new to ruby
+  # db = Mongo::Connection.new("mongohq-url", port).db("database")
+  # auth = db.authenticate("username", "password")
+  # coll = db.collection("emails")
+  #  
+  # # sinatra-book-contrib
+  # conn = Mongo::Connection.new("localhost", 27017).db
+  # set :mongo_connection, conn
+  # set :mongo_db,         conn.db('test')
+  #  
+  # # sinatra_mongo_base
+  # db = Mongo::Connection.new("flame.mongohq.com", 27020).db("your_database")
+  # auth = db.authenticate("your_user", "your_password")
+  # coll = db.collection("your_collection")
+
+  # mongodb://fred:foobar@localhost/baz
+  # mongodb://heroku:6da6skmzn3ymi9kqmnsixg@flame.mongohq.com:27107/app554007
+  # mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]][/[database][?options]]
+  
+  # specify env variable on command line for development; production uses vars listed in heroku config
+  uri = URI.parse(ENV['MONGOHQ_URL'])
+  # you don't need to specify auth because it's already in the env var
+  conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
+  db = conn.db(uri.path.gsub(/^\//, ''))
+  
+  # access a specific collection
+  set :db, db
+  # set :coll, db.collection("your_collection")
 end
 
 get '/collections/?' do
