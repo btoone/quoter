97900874c8cd5e478ca8bd4a1fb131d3e12a793c
Brandon Toone <xsonih@gmail.com>
2011-06-07 10:53:38 -0600

    Connect to mongohq

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require 'sinatra'
+require 'mongo'
+require 'uri'
+
+configure do
+    uri = URI.parse(ENV['MONGOHQ_URL'])
+    # you don't need to specify auth because it's already in the env var
+    conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])    
+    db = conn.db(uri.path.gsub(/^\//, ''))
+    
+    # access a specific collection
+    set :db, db
+    set :coll, db.collection("your_collection")
+end
+
+get '/collections/?' do
+  settings.db.collection_names
+end
 
 get '/' do
   @time = Time.now
